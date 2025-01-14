7ee6caf38ea6ac47230321bde9aa68d3f84e6c23
saj.matthew <saj.matthew@glue-iq.com>
2025-01-14 16:09:11 -0500

    app insights update

--- src/features/main-menu/menu-store.tsx
+++ src/features/main-menu/menu-store.tsx
@@ -1,6 +1,6 @@
 import { proxy, useSnapshot } from "valtio";
 class Menu {
-  public isMenuOpen: boolean;
+  public isMenuOpen: boolean = false;
 
   constructor() {
     this.isMenuOpen = true;
@@ -14,5 +14,6 @@ class Menu {
 export const menuStore = proxy(new Menu());
 // Hook to use the menu state
 export const useMenuState = () => {
+  console.log(menuStore.isMenuOpen);
   return useSnapshot(menuStore);
 };
